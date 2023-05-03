35df34ade76a38f4b83e68e05f6106281e5177ae
Joris Loemba <jorisloemba@gmail.com>
2023-05-03 17:07:04 +0200

    authentification: check if user is already registered

--- controllers/user.js
+++ controllers/user.js
@@ -1,54 +1,52 @@
-const User = require('../models').User;
-var jwt = require('jsonwebtoken');
+const User = require("../models").User;
+var jwt = require("jsonwebtoken");
 
 module.exports = {
+  register: function (req, res, next) {
+    try {
+      if (req.body.email) {
+        User.findOne({ where: { email: req.body.email } }).then((result) => {
+          console.log(result);
+          if (result) res.status(409).json({message:"existe déjà"});
+          else {
+            const { email } = req.body;
+            User.create({ email: email, isAdmin: false }).then(
+              (createdUser) => {
+                res.status(201).json(createdUser);
+              }
+            );
+          }
+        });
+      } else res.status(402).json("Payload 'email' for a user creation");
+    } catch (error) {
+      res.status(error.response.status);
+      return res.send(error.message);
+    }
+  },
+  isUserExistByEmail: function (req, res, next) {
+    try {
+      if (req.query.email) {
+        User.findOne({ where: { email: req.query.email } }).then(
+          (foundUser) => {
+            if (foundUser) {
+              const { id, email } = foundUser;
+              const token = jwt.sign(
+                { user_id: id, email },
+                process.env.TOKEN_KEY,
+                {
+                  expiresIn: "30d",
+                }
+              );
 
-    register: function(req, res, next) {
-        try {
-            if (req.body.email) {
-                User.findOne({ where: { email: req.body.email } })
-                    .then(result => {
-                        if (result) res.status(200).json("existe déjà")
-                        const { email } = req.body
-
-                        User.create({ email: email,isAdmin:false })
-                            .then(createdUser => {
-                                res.status(201).json(createdUser);
-                            })
-                    })
-            }
-            else res.status(402).json("Payload 'email' for a user creation")
-        } catch (error) {
-            res.status(error.response.status)
-            return res.send(error.message);
-        }
-    },
-    isUserExistByEmail: function(req, res, next) {
-        try {
-            if (req.query.email) {
-                User.findOne({ where: { email: req.query.email } })
-                    .then(foundUser => {
-                        if (foundUser) {
-                            const { id, email } = foundUser
-                            const token = jwt.sign(
-                                { user_id: id, email },
-                                process.env.TOKEN_KEY,
-                                {
-                                    expiresIn: "2h",
-                                }
-                            );
-
-                            foundUser.token = token;
-                            res.status(200).json(foundUser);
-                        }
-                        else res.status(200).json(false)
-                    })
-            }
-            else res.status(402).json("Payload 'email' for a user checking")
-        } catch (error) {
-            res.status(error.response.status)
-            return res.send(error.message);
-        }
-    },
-
-}
+              foundUser.token = token;
+              res.status(200).json(foundUser);
+            } else res.status(200).json(false);
+          }
+        );
+      } else res.status(402).json("Payload 'email' for a user checking");
+    } catch (error) {
+      res.status(error.response.status);
+      return res.send(error.message);
+    }
+  },
+};
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,22 +1,26 @@
-var jwt = require('jsonwebtoken');
-const User = require('../models').User;
+var jwt = require("jsonwebtoken");
+const User = require("../models").User;
 
 function verifyToken(req, res, next) {
-  var token = req.headers['x-access-token'];
+  var token = req.headers["x-access-token"];
   if (!token)
-    return res.status(403).send({ auth: false, message: 'No token provided.' });
-  jwt.verify(token, process.env.TOKEN_KEY, function(err, decoded) {
-    User.findOne({ where: { email: decoded.email } })
-      .then(foundUser => {
-        if (!foundUser.isActived) return res.status(500).send({ auth: false, message: 'Your account is not activated' });
-      })
+    return res.status(403).send({ auth: false, message: "No token provided." });
+  jwt.verify(token, process.env.TOKEN_KEY, function (err, decoded) {
+    if (decoded) {
+      User.findOne({ where: { email: decoded.email } }).then((foundUser) => {
+        if (!foundUser.isActived)
+          return res
+            .status(500)
+            .send({ auth: false, message: "Your account is not activated" });
+      });
+    } else return res.status(403).send({ auth: false, message: "Expired session" });
     if (err)
-      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate token." });
     req.userId = decoded.id;
     next();
   });
 }
 
 module.exports = verifyToken;
-
-
